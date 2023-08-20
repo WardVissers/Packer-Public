5e890952737b9d00f4e413d2ff9607abacfd0d57
Ward Vissers <39764253+WardVissers@users.noreply.github.com>
2023-08-20 16:46:50 +0200

    Delete featuresondemandv2.ps1

--- setup/featuresondemandv2.ps1
+++
@@ -1,7 +0,0 @@
-### Enable WinRM
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-# Add-WindowsCapability -Online -Name "ServerCore.AppCompatibility~~~~0.0.1.0"
-Get-WindowsCapability -Name "ServerCore.AppCompatibility~~~~0.0.1.0" -Online
-Start-Sleep 10
-Get-WindowsCapability -Name "ServerCore.AppCompatibility~~~~0.0.1.0" -Online | Add-WindowsCapability  -Name "ServerCore.AppCompatibility~~~~0.0.1.0" -Online
-
