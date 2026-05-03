a020968db0367019af4f9f9841129ca0076b45f1
Ward Vissers <39764253+WardVissers@users.noreply.github.com>
2026-05-03 18:17:33 +0200

    Delete Windows directory

--- Windows/VirtualMachineGuestOsIdentifier over PowerCLI.ps1
+++
@@ -1,6 +0,0 @@
-﻿$vcenter= read-host "What is the name of the vCenter"
-connect-viserver $vcenter
-
-[VMware.Vim.VirtualMachineGuestOsIdentifier].GetEnumValues()
-
-Disconnect-VIServer * -Confirm:$false
--- Windows/setup/windowsupdate.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-#Install PS Windows Update Module
-
-Get-PackageProvider -name nuget -force
-Install-Module PSWindowsUpdate -confirm:$false -force
-Get-WindowsUpdate -MicrosoftUpdate -install -IgnoreUserInput -acceptall -AutoReboot | Out-File -filepath 'c:\temp\windowsupdate.log' -append
-
